7717a20f9ff59e31814874b012a5818c4b855f36
jack <graham@friends.dds.mil>
2019-02-18 23:28:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/irisnav/BaseActivity.java
@@ -0,0 +1,80 @@
+package com.example.irisnav;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class BaseActivity extends AppCompatActivity {
+
+    private static final String SELECTED_ITEM = "arg_selected_item";
+
+    private BottomNavigationView mBottomNav;
+    private int mSelectedItem;
+    private FragmentManager mFragmentManager;
+
+    private HomeFragment homeFragment;
+    private IrisFragment irisFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.base_activity_layout);
+
+        homeFragment = new HomeFragment();
+        irisFragment = new IrisFragment();
+
+        mBottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                selectFragment(item);
+                Log.d("BASEACTIVITY", "onNavigationItemSelected()");
+                return true;
+            }
+        });
+
+        MenuItem selectedItem;
+        if (savedInstanceState != null) {
+            mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
+            selectedItem = mBottomNav.getMenu().findItem(mSelectedItem);
+        } else {
+            selectedItem = mBottomNav.getMenu().getItem(0);
+        }
+        selectFragment(selectedItem);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SELECTED_ITEM, mSelectedItem);
+        super.onSaveInstanceState(outState);
+    }
+
+    private void selectFragment(MenuItem item) {
+        Fragment frag = null;
+        // init corresponding fragment
+        switch (item.getItemId()) {
+            case R.id.home_button:
+                frag = homeFragment;
+                break;
+            case R.id.iris_button:
+                frag = irisFragment;
+                break;
+        }
+
+        if (frag != null) {
+            mFragmentManager = this.getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, frag);
+            fragmentTransaction.commit();
+        }
+        Log.d("BASEACTIVITY", "fragment: " + frag.toString());
+
+    }
+}
